a15f5e7997c388de128241b81dc80320be841b3b
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-02-26 16:59:23 +0100

    Swagger for login and register

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,25 @@ const register = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const admin = await Admin.findOne({ email: email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const isMatch = await bcrypt.compare(password, admin.password)
+            if (!isMatch) {
+                res.status(400).send("Incorrect password")
+            } else {
+                const token = jwt.sign({ email: admin.email }, process.env.JWT_SECRET, { expiresIn: '5y' })
+                res.status(200).send({email: admin.email, token: token})
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
 
-
-module.exports = {register}
+module.exports = {register, login}
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { register, login, forgotPassword, resetPassword, revokeAccess, grantAccess, deleteUser, getUsers } = require('../controllers/admin')
 
 router.post('/register', register)
-// router.post('/login', login)
+router.post('/login', login)
 // router.post('/forgot-password', forgotPassword)
 // router.post('/reset-password', resetPassword)
 // router.get('/users', getUsers)
