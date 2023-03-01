50b2879d1f67778695ae25559e6b2a38affb0bd0
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-03-01 19:51:33 +0100

    Admin almost done

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Admin = require('../models/admin')
 const Driver = require('../models/driver')
+const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 require('dotenv').config()
@@ -94,8 +95,142 @@ const deleteDriver = async (req, res) => {
     }
 }
 
+const getUsers = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const users = await User.find()
+            res.status(200).send(users)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const getUser = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const user = await User.findOne({ _id: req.params.id })
+            res.status(200).send(user)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+
+}
+
+const revokeAccess = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const user = await User.findOne({ _id: req.params.id })
+            if (!user) {
+                res.status(404).send("User not found")
+            } else {
+                user.access = false
+                await user.save()
+                res.status(200).send("Access revoked")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const grantAccess = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const user = await User.findOne({ _id: req.params.id })
+            if (!user) {
+                res.status(404).send("User not found")
+            } else {
+                user.access = true
+                await user.save()
+                res.status(200).send("Access granted")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const user = await User.findOne({ _id: req.params.id })
+            if (!user) {
+                res.status(404).send("User not found")
+            } else {
+                await User.deleteOne({ _id: req.params.id })
+                res.status(200).send("User deleted")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const getTrips = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const trips = await Trip.find()
+            res.status(200).send(trips)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const getTrip = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const admin = await Admin.findOne({ email: decoded.email })
+        if (!admin) {
+            res.status(404).send("Admin not found")
+        } else {
+            const trip = await Trip.findOne({ _id: req.params.id })
+            res.status(200).send(trip)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
 
 
-
-
-module.exports = {register, login, getDrivers, getDriver, deleteDriver}
+module.exports = {register, login, getDrivers, getDriver, deleteDriver, getUsers, getUser, revokeAccess, grantAccess, deleteUser, getTrips, getTrip}
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const { register, login, getDrivers, getDriver, deleteDriver } = require('../controllers/admin')
+const { register, login, getDrivers, getDriver, deleteDriver, getUsers, getUser, revokeAccess, grantAccess, deleteUser, getTrips, getTrip } = require('../controllers/admin')
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/drivers', getDrivers)
 router.get('/driver/:id', getDriver)
 router.delete('/driver/:id', deleteDriver)
-
-// router.post('/forgot-password', forgotPassword)
-// router.post('/reset-password', resetPassword)
-// router.get('/users', getUsers)
-// router.patch('/revoke-access/:id', revokeAccess)
-// router.patch('/grant-access/:id', grantAccess)
-// router.delete('/delete-user/:id', deleteUser)
+router.get('/users', getUsers)
+router.get('/user/:id', getUser)
+router.delete('/user/:id', deleteUser)
+router.get('/trips', getTrips)
+router.get('/trip/:id', getTrip)
+router.post('/revoke', revokeAccess)
+router.post('/grant', grantAccess)
 
 module.exports = router
